a666b7b21adb974c4f81008b0e3725521a4d7417
Rahul Tuladhar <rt4hc@virginia.edu>
2019-10-05 15:59:26 -0400

    added link to personal pickled model and for robots:

--- app/server.py
+++ app/server.py
@@ -9,10 +9,13 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+
+# Use this link to change google drive links 
+# https://www.wonderplugin.com/online-tools/google-drive-direct-link-generator/
+export_file_url = 'https://www.dropbox.com/s/1v3l4kjitohixye/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bipedal', 'drone', 'quadpedal']
 path = Path(__file__).parent
 
 app = Starlette()
